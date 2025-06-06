236b27e20b9a86dde1f68f3a72ad1f1c5a58d70d
masaniappanrahul <bewithrahul07@gmail.com>
2025-06-06 16:31:20 +0530

    dom added

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+// console.log("hello")
+  //  var a;
+  //  a=10;
+  //  var b=20;
+   // console.log(a*b);
+
+// odd or even
+//var number=16;
+//if(number %2==0){
+  //  console.log("even")
+  //function sum(){
+  //  console.log(a+b)
+ // }
+//var sum=(a,b)=>{
+  //  return a+b;
+//}
+//console.log(sum(10,20))
+
+//var arr=[10,20,30,40];
+//console.log(arr)
+//var arr2=[40,50];
+//var arr2=[...arr,40,50];
+//console.log(arr2)
+
+//var marks =[90,100,89,79,45];
+//var [m1,m2,m3,m4,m5] =marks;
+//console.log(m1,m2)
+
+var a=10
+var b=12
+var sum=a+b;
+console.log(a,"+",b,"=",sum)
+console.log(`${a}+${b}=${sum}`)
